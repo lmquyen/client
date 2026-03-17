404848a1bb7a73fe40862d5d3ebd217e5859d5d3
quyen.le <quyen.le@persol-pt.co.jp>
2026-03-17 12:07:52 +0700

    Demo

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,12 @@ import { useEffect, useState } from 'react';
 
 export default function Home() {
   const [msg, setMsg] = useState('Đang kết nối server...');
+  //const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000/api/data';
+  //const API_URL = 'http://localhost:3000';
+  const API_URL = 'https://server-gdh6.onrender.com';
 
   useEffect(() => {
-    fetch('http://localhost:3000') // Gọi tới NestJS mặc định
+    fetch(API_URL) // Gọi tới NestJS mặc định
       .then(res => res.text())
       .then(data => setMsg(data))
       .catch(() => setMsg('Server chưa bật!'));
